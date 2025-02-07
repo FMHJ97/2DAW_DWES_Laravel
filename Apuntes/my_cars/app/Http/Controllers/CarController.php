f3545b58b1f5334471b3693941c79bd61b7ac7bf
Francisco Manuel Hernández <francisco-h-j@hotmail.com>
2025-02-07 10:29:45 +0100

    feat(my_cars): files updated

--- Apuntes/my_cars/app/Http/Controllers/CarController.php
+++ Apuntes/my_cars/app/Http/Controllers/CarController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Car;
+use App\Models\User;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,7 +15,9 @@ class CarController extends Controller
      */
     public function index()
     {
-        return view('cars.index');
+        $user = User::find(Auth::id()); // Get the authenticated user.
+        $cars = $user->cars()->OrderBy('plate')->get(); // Get the cars of the authenticated user.
+        return view('cars.index')->with('cars', $cars);
     }
 
     /**
@@ -30,7 +34,8 @@ class CarController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'plate' => 'required|unique:cars,plate', // The plate must be unique in the cars table.
+            // The plate must be unique in the cars table, except for the current car.
+            'plate' => 'required|unique:cars,plate,NULL,id,deleted_at,NULL',
             'brand' => 'required',
             'model' => 'required',
             'year' => 'required|integer',
@@ -40,22 +45,34 @@ class CarController extends Controller
             'price' => 'required|numeric|min:0',
         ]);
 
-        $new_car = new Car();
-        $new_car->plate = $request->plate;
-        $new_car->brand = $request->brand;
-        $new_car->model = $request->model;
-        $new_car->year = $request->year;
-        $new_car->color = $request->color;
-        $new_car->revision_date = $request->revision_date;
-        $new_car->price = $request->price;
-        $new_car->user_id = Auth::id(); // Get the id of the authenticated user.
+        try {
 
-        // Save the image in the public folder.
-        $name_image = time() . "_" . $request->file('image')->getClientOriginalName(); // Get the name of the image.
-        $new_car->image = $name_image; // Save the name of the image in the database.
+            $new_car = new Car();
+            $new_car->plate = $request->plate;
+            $new_car->brand = $request->brand;
+            $new_car->model = $request->model;
+            $new_car->year = $request->year;
+            $new_car->color = $request->color;
+            $new_car->revision_date = $request->revision_date;
+            $new_car->price = $request->price;
+            $new_car->user_id = Auth::id(); // Get the id of the authenticated user.
 
-        // Save the car in the database.
-        $new_car->save();
+            // Save the image in the public folder.
+            $name_image = time() . "_" . $request->file('image')->getClientOriginalName(); // Get the name of the image.
+            $new_car->image = $name_image; // Save the name of the image in the database.
+
+            // Save the car in the database.
+            $new_car->save();
+
+            // Move the image to the public folder.
+            $request->file('image')->storeAs('img_cars', $name_image);
+
+            // Redirect to the index view.
+            return to_route('cars.index')->with('msg', 'Car created successfully.');
+            // return view('cars.index')->with('msg', 'Car created successfully.');
+        } catch (QueryException $qe) {
+            return to_route('cars.index')->with('msg', 'Error creating the car. Try again later.');
+        }
     }
 
     /**
