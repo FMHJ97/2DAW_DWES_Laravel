da40a4b3a84cff2416b82fd18cb77303028313b5
Francisco Manuel Hernández <francisco-h-j@hotmail.com>
2025-02-10 13:09:56 +0100

    feat(my_cars): files updated

--- Apuntes/my_cars/app/Http/Controllers/CarController.php
+++ Apuntes/my_cars/app/Http/Controllers/CarController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class CarController extends Controller
 {
@@ -80,7 +81,8 @@ class CarController extends Controller
      */
     public function show(Car $car)
     {
-        //
+        $url = 'storage/img_cars/';
+        return view('cars.show')->with('mycar', $car)->with('url', $url);
     }
 
     /**
@@ -88,7 +90,8 @@ class CarController extends Controller
      */
     public function edit(Car $car)
     {
-        //
+        $url = 'storage/img_cars/';
+        return view('cars.edit')->with('mycar', $car)->with('url', $url);
     }
 
     /**
@@ -96,7 +99,49 @@ class CarController extends Controller
      */
     public function update(Request $request, Car $car)
     {
-        //
+        $request->validate([
+            // The plate must be unique in the cars table, except for the current car.
+            'plate' => 'required|unique:cars,plate,' . $car->id . ',id,deleted_at,NULL',
+            'brand' => 'required',
+            'model' => 'required',
+            'year' => 'required|integer',
+            'color' => 'required',
+            'revision_date' => 'required|date',
+            'image' => 'image',
+            'price' => 'required|numeric|min:0',
+        ]);
+
+        try {
+
+
+            $car->plate = $request->plate;
+            $car->brand = $request->brand;
+            $car->model = $request->model;
+            $car->year = $request->year;
+            $car->color = $request->color;
+            $car->revision_date = $request->revision_date;
+            $car->price = $request->price;
+
+            // If the user uploads a new image.
+            if (is_uploaded_file($request->file('image'))) {
+                // Delete the old image.
+                Storage::delete('img_cars/' . $car->image);
+                // Save the image in the public folder.
+                $name_image = time() . "_" . $request->file('image')->getClientOriginalName(); // Get the name of the image.
+                $car->image = $name_image; // Save the name of the image in the database.
+                // Move the image to the public folder.
+                $request->file('image')->storeAs('img_cars', $name_image);
+            }
+
+            // Save the car in the database.
+            $car->save();
+
+            // Redirect to the index view.
+            return to_route('cars.index')->with('msg', 'Car UPDATED successfully.');
+            // return view('cars.index')->with('msg', 'Car created successfully.');
+        } catch (QueryException $qe) {
+            return to_route('cars.index')->with('msg', 'Error creating the car. Try again later.');
+        }
     }
 
     /**
@@ -104,6 +149,8 @@ class CarController extends Controller
      */
     public function destroy(Car $car)
     {
-        //
+        $car->delete();
+        // Storage::delete('img_cars/' . $car->image); // Delete the image of the car.
+        return to_route('cars.index')->with('msg', 'Car deleted successfully.');
     }
 }
--- Apuntes/my_cars/resources/views/cars/index.blade.php
+++ Apuntes/my_cars/resources/views/cars/index.blade.php
@@ -6,7 +6,7 @@
         @if (session('msg'))
             <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
                 role="alert">
-                <span class="font-medium">Well done!</span> {{ session('msg') }}
+                {{ session('msg') }}
             </div>
         @endif
     </x-slot>
@@ -15,7 +15,84 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table class="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400">
+                            <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        <div class="flex items-center">
+                                            Matrícula
+                                            <svg class="w-3 h-3 ms-1.5" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                viewBox="0 0 24 24">
+                                                <path
+                                                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                                            </svg>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        <div class="flex items-center">
+                                            Marca
+                                            <svg class="w-3 h-3 ms-1.5" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                viewBox="0 0 24 24">
+                                                <path
+                                                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                                            </svg>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        <div class="flex items-center">
+                                            Modelo
+                                            <svg class="w-3 h-3 ms-1.5" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                viewBox="0 0 24 24">
+                                                <path
+                                                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                                            </svg>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        <div class="flex items-center">
+                                            Operaciones
+                                        </div>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($cars as $car)
+                                    <tr class="bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ $car->plate }}
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            {{ $car->brand }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $car->model }}
+                                        </td>
+                                        <td class="inline-flex px-6 py-4">
+                                            <a href="{{ route('cars.show', ['car' => $car->id]) }}"><button
+                                                    type="button"
+                                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Detalles</button></a>
+                                            <a href="{{ route('cars.edit', ['car' => $car->id]) }}"><button
+                                                    type="button"
+                                                    class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Editar</button></a>
+                                            <form action="{{ route('cars.destroy', ['car' => $car->id]) }}"
+                                                method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit"
+                                                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Eliminar</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
